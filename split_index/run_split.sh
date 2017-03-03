f052d2e13f842f1348df98d95a475b8dc494c3a8
xiongxs <461087402@qq.com>
2017-03-03 19:59:07 +0800

    0303_splitindex

---
+++ split_index/run_split.sh
@@ -0,0 +1,2 @@
+#for i in `cat list`; do echo -e "$i,\c";done
+/WPSnew/lilin/software/anaconda/bin/python index_split.py -t 12 -l 150 -b 0 pool2-160425_HNH5WCCXX_L2_1.fq.gz pool2-160425_HNH5WCCXX_L2_2.fq.gz TTAGGC,TGACCA,CAGATC,GGCTAC,AGTTCC,CCGTCC,GTCCGC,GTGAAA,GTGGCC,ATGAGC,AGAGTA,ACCTTA,CGCCAA,GTTACC,ATACAC,GCATTC,TACTCT
